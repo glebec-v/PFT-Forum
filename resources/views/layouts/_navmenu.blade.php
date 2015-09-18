dd38e46ddcdd79d2ed683e9282225631efe66cdb
Vitaliy <ist70@mail.ru>
2015-09-18 15:14:03 +0400

    Вынос меню в отдельный файл из шаблона

---
+++ resources/views/layouts/_navmenu.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="background-color: #F7B10A  ">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">PFT-Forum</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Главная</a></li>
+                <li><a href="#">Форумы</a></li>
+                <li><a href="#">Новости</a></li>
+            </ul>
+            <a href="#" class="navbar-right">
+                <button class="btn btn-default btn-sm navbar-btn">Войти</button>
+            </a>
+            <p class="navbar-text navbar-right" style="padding-right: 10px;">Здравствуйте, ник!</p>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,31 +9,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-default navbar-static-top" role="navigation" style="background-color: #F7B10A  ">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">PFT-Forum</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Главная</a></li>
-                <li><a href="#">Форумы</a></li>
-                <li><a href="#">Новости</a></li>
-            </ul>
-            <a href="#" class="navbar-right">
-                <button class="btn btn-default btn-sm navbar-btn">Войти</button>
-            </a>
-            <p class="navbar-text navbar-right" style="padding-right: 10px;">Здравствуйте, ник!</p>
-        </div>
-    </div>
-</nav>
+@include('layouts._navmenu')
 
 <div class="container">
     <div class="col-md-10">
@@ -60,7 +36,7 @@
 {!! Html::script('http://code.jquery.com/jquery-1.11.3.min.js') !!}
 {!! Html::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js') !!}
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-{!! Html::script ("resources/assets/js/ie10-viewport-bug-workaround.js") !!}
+{!! Html::script ("/js/ie10-viewport-bug-workaround.js") !!}
 
 </body>
 </html>
