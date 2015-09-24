29388824ddea82dc694c3912290c9f054cfabca5
Vitaliy <ist70@mail.ru>
2015-09-24 14:36:16 +0400

    Шапка сайта, адаптив, поиск

--- resources/views/layouts/_bredcrampsandfind.blade.php
+++ resources/views/layouts/_bredcrampsandfind.blade.php
@@ -1,5 +1,6 @@
 <div class="container">
-    <div id="secondnav" class="navbar navbar-default clearfix col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="row">
+    <div class="navbar navbar-default clearfix col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="col-lg-8 col-md-8 hidden-sm hidden-xs">
             <ol class="breadcrumb">
                 <li><a href="#">Главная</a></li>
@@ -9,22 +10,12 @@
             </ol>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-            <ul id='toplinks' class="list-inline">
-                <li class='right' style='padding-top: 3px; padding-bottom: 3px;'>
-                    <div id='search' class='right'>
-                        <form action="http://forum-engineering.ru/index.php?app=core&amp;module=search&amp;do=search&amp;fromMainBar=1"
-                              method="post" id='search-box'>
-                            <fieldset>
-                                <label for='main_search' class='hide'>Поиск</label>
-                                <a href='http://forum-engineering.ru/index.php?app=core&amp;module=search&amp;search_in=forums'
-                                   title='Расширенный поиск' accesskey='4' rel="search" id='adv_search' class='right'>Расширенный</a>
-			<span id='search_wrap' class='right'>
-				<input type='text' id='main_search' name='search_term' class='inactive' size='17' tabindex='100'/>
-				<span class='choice ipbmenu clickable' id='search_options' style='display: none'></span>
-				<input type='submit' class='submit_input clickable' value='Поиск'/>
-			</span>
-
-                            </fieldset>
+            <ul class="list-inline pull-right">
+                <li>
+                    <div class="pull-right">
+                        <form action="#" method="post" class="form-search">
+                            <input type="text" class="input-sm search-query">
+                            <button type="submit" class="btn">Поиск</button>
                         </form>
                     </div>
                 </li>
@@ -33,5 +24,6 @@
         <div id="timer">
 До конца света осталось :
         </div>
+    </div>
     </div>
 </div>
