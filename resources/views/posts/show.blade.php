70d834dd14f03dbc7081faec8e8b17feedb1baf3
glebec <gvorotilov@gmail.com>
2015-10-03 01:10:20 +0300

    TODO: исправить контроллеры и шаблоны под новую структуру БД

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
 @section('content')
     <h2>{{ $forumpost->title }}</h2>
-    <p>{{ $forumpost->content->body }}</p>
-    <pre>{{ $forumpost->content->code }}</pre>
+    <p>{{ $forumpost->body }}</p>
+    <pre>{{ $forumpost->code }}</pre>
     <hr/>
     @if ( $forumpost->pictures->count() > 0)
         <ul>
