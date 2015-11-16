54dd5d36af9060e7c49425378e21c798a6af68ac
glebec-P67A <gvorotilov@gmail.com>
2015-11-16 15:39:43 +0300

    работает выборка категорий в navmenu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View()->composer('partials.navmenu', function($view){
+            $view->with('categories', \App\Models\Category::all());
+        });
     }
 
     /**
--- resources/views/partials/navmenu.blade.php
+++ resources/views/partials/navmenu.blade.php
@@ -1,17 +1,12 @@
 <li class="dropdown active">
-    <a href="#" class="dropdown-toggle">Категории <b class="caret"></b></a>
+    <a href="/categories/" class="dropdown-toggle">Категории <b class="caret"></b></a>
     <ul class="dropdown-menu">
-        <li><a href="#">Достать из БД существующие категории</a></li>
-        <li><a href="#">Two Column</a></li>
-        <li><a href="#">Three Column</a></li>
-        <li><a href="#">Documentation</a></li>
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle">Dropdown Item &nbsp;&raquo;</a>
-            <ul class="dropdown-menu sub-menu">
-                <li><a href="#">Dropdown Item</a></li>
-                <li><a href="#">Dropdown Item</a></li>
-                <li><a href="#">Dropdown Item</a></li>
-            </ul>
-        </li>
+        @foreach( $categories as $category)
+            <li>
+                @if ($category->posts()->count() > 0)
+                    {!! link_to('posts/thread/'.$category->id, $category->name) !!}
+                @endif
+            </li>
+        @endforeach
     </ul>
 </li>
