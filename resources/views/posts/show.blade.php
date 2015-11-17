c98b9130cc95361b3e036495b914dd9083b12c86
glebec-P67A <gvorotilov@gmail.com>
2015-11-17 16:09:46 +0300

    форма сообщения, разработка

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.master')
 @section('content')
-    <h2>{{ $forumpost->title }}</h2>
-    <p>{{ $forumpost->body }}</p>
-    <pre>{{ $forumpost->code }}</pre>
-    <hr/>
-    @if ($forumpost->pictures->count() > 0)
-        @foreach ($forumpost->pictures as $picture)
-            <img src="{{ GlideImage::load($picture->name)->modify(['w' => 100]) }}"/>
-        @endforeach
-    @endif
+    <div class="divPanel page-content">
+        <div class="row-fluid">
+            <div class="span10">
+                <h3>{{ $forumpost->title }}</h3>
+                <p>{{ $forumpost->body }}</p>
+                <span>Code:</span>
+                <pre>{{ $forumpost->code }}</pre>
+                <hr/>
+                @if ($forumpost->pictures->count() > 0)
+                    @foreach ($forumpost->pictures as $picture)
+                        <img src="{{ GlideImage::load($picture->name)->modify(['w' => 100]) }}"/>
+                    @endforeach
+                @endif
+            </div>
+            <div class="span2">
+                <div class="line-separator"></div>
+                <p><i class="icon-user"></i> {{ $forumpost->user->name }}</p>
+                <p><i class="icon-calendar"></i> {{ \Carbon\Carbon::parse($forumpost->updated_at)->format('d-m-y H:i') }}</p>
+                <p><a class="btn btn-success btn-small" href="/post/{{$forumpost->id}}/edit">Редактировать</a></p>
+                <p><a class="btn btn-primary btn-small" href="/posts/create-next/{{ $forumpost->category_id }}/{{$forumpost->id}}">Комментировать</a></p>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('profit_links')
-    <p>
-        {!! link_to_action('PostsThreadsController@getCreateNext', 'Комментировать', [
-            'category_id' => $forumpost->category_id,
-            'parent_id' => $forumpost->id
-        ]) !!}
-    </p>
     @parent
 @endsection
